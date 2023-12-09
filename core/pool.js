bd1dc8afc24e12cb991363ed5c7d9ff0729529b1
Shubzz <19619948+Shubzz-02@users.noreply.github.com>
2023-12-09 23:58:00 +0530

    Initial commit

---
+++ core/pool.js
@@ -0,0 +1,25 @@
+const util = require('util');
+const mysql = require('mysql');
+/**
+ * Connection to the database.
+ *  */
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'db4free.net',
+    user: 'azure_test', // use your mysql username.
+    password: '1234567890', // user your mysql password.
+    database: 'www_test'
+});
+
+pool.getConnection((err, connection) => {
+    if(err) 
+        console.error("Something went wrong connecting to the database ...");
+    
+    if(connection)
+        connection.release();
+    return;
+});
+
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
